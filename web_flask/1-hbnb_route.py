320dc25cfa7d19e371bc6d58aa142f1d3878f114
johhnMunene <Edwardsjohnny64@gmail.com>
2024-01-26 05:39:16 -0600

    web application must be listening on 0.0.0.0, port 5000 Routes: : display “Hello HBNB!” hbnb: display “HBNB”

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" 
-Web application must be listening on 0.0.0.0, port 5000
+"""Web application must be listening on 0.0.0.0, port 5000
 Routes:
     /: display “Hello HBNB!”
     /hbnb: display “HBNB”
