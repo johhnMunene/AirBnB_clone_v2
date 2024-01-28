3b5f394342c8ec72245c3f924207ab9f62920364
johhnMunene <Edwardsjohnny64@gmail.com>
2024-01-28 14:19:18 -0600

    a script that starts a Flask web application:
        Your web application must be listening on 0.0.0.0, port 5000
        Routes:
            /: display “Hello HBNB!”
            /hbnb: display “HBNB”
            /c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
            /python/<text>: display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
                The default value of text is “is cool”
        You must use the option strict_slashes=False in your route definition

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" a script that starts a Flask web application:
+"""
+A script that starts a Flask web application:
 
-    Your web application must be listening on 0.0.0.0, port 5000
-    Routes:
-        /: display “Hello HBNB!”
-        /hbnb: display “HBNB”
-        /c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-        /python/<text>: display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-            The default value of text is “is cool”
-    You must use the option strict_slashes=False in your route definition
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+    /c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+    /python/<text>: display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+        The default value of text is “is cool”
+You must use the option strict_slashes=False in your route definition
 """
 from flask import Flask
 
@@ -20,17 +21,18 @@ def hello_hbnb():
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return ' HBNB'
+    return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-    return "C {}".format(text.replace('_', ' '))
+    return 'C {}'.format(text.replace('_', ' '))
 
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
 def python_route(text="is cool"):
-    return "Python {}".format(text.replace('_', ''))
+    return 'Python {}'.format(text.replace('_', ' '))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
 
